3de5cadc18c81dba86ff01f88fcad7b82f690076
Mikhawa <michaeljpitz@gmail.com>
2022-05-09 14:17:00 +0200

    for in articles by section

--- controller/routerController.php
+++ controller/routerController.php
@@ -54,8 +54,38 @@ elseif (isset($_GET['section'])):
 
     endif;
 
-// ICI
+// Détail d'un article
+elseif (isset($_GET['article'])):
+    /* si slug trouvé, contient un tableau associatif
+    $theSectionDatas = $thesectionManager->SelectOneThesectionBySlug($_GET['section']);
 
+    // sinon le résultat est un string
+    if (is_string($theSectionDatas)):
+
+        // si elle est vide (pas de section ou PROD est à true)
+        if (empty($theSectionDatas)) {
+            $theSectionDatas = "Rubrique inexistante";
+        }
+
+        // appel de l'erreur 404
+        echo $twig->render('public/error404.html.twig', [
+            'menu'    => $thesectionMenu,
+            'message' => $theSectionDatas,
+        ]);
+    else:
+
+        // Selection des articles de la section
+        $articles = $thearticleManager->thearticleSelectAllFromSection($theSectionDatas['idthesection']);
+
+        // affichage de le section
+        echo $twig->render('public/section.html.twig', [
+            'menu'     => $thesectionMenu,
+            'section'  => $theSectionDatas,
+            'articles' => $articles,
+        ]);
+
+    endif;
+*/
 // contact
 elseif (isset($_GET['contact'])):
     if (isset($_POST["name"], $_POST["email"], $_POST["message"])) {
