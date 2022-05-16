f9b981b72b9a8e493c67f1c124c4061041925ec4
Mikhawa <michaeljpitz@gmail.com>
2022-05-16 14:34:01 +0200

    die

--- controller/private/privateRouterController.php
+++ controller/private/privateRouterController.php
@@ -5,6 +5,7 @@ use NewsWeb\Manager\theuserManager;
 if (isset($_GET["disconnect"]) || $_SESSION["idSession"] !== session_id()) {
     theuserManager::disconnect();
     header("Location: ./");
+    die();
 } else {
     echo "<h1>You are logged in!</h1><pre>";
     var_dump($_SESSION);
