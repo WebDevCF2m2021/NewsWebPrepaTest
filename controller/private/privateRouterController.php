e728844fdfaa672e588c1db59e31c47131e874fa
Mikhawa <michaeljpitz@gmail.com>
2022-05-18 16:32:47 +0200

    private.template on load....

--- controller/private/privateRouterController.php
+++ controller/private/privateRouterController.php
@@ -7,8 +7,11 @@ if (isset($_GET["disconnect"]) || $_SESSION["idSession"] !== session_id()) {
     header("Location: ./");
     die();
 } else {
+    /*
     echo "<h1>You are logged in!</h1><pre>";
     var_dump($_SESSION);
     echo "</pre><p><a href='?disconnect'>Logout?</a></p>";
     // ici l'appel de la vue Twig (private homepage)
+    */
+    echo $twig->render("private/private.template.html.twig");
 }
