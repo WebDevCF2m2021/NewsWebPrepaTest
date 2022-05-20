b50658c5808a60da02f145c8f28f76e77f3a2789
QuentinFayt <quentin.fayt.p@gmail.com>
2022-05-20 15:33:24 +0200

    article resume display + sections display

--- controller/private/writer/writerRouterController.php
+++ controller/private/writer/writerRouterController.php
@@ -1,9 +1,13 @@
 <?php
 
-if ($_GET) {
+use NewsWeb\Manager\thesectionManager;
+
+$sectionManager = new thesectionManager($connectMyPDO);
+if (isset($_GET["addArticle"])) {
     echo $twig->render("private/articleForm.html.twig", [
         'username' => $_SESSION['userLogin'],
         'session'  => $_SESSION,
+        "sections" => $sectionManager->SelectAllThesection(),
     ]);
 }
 else {
